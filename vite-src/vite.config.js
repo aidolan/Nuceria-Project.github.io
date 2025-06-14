cbcb168f66a1d799a18c8b5dd966b4b2f97c943a
Jacob <contentdeleted@gmail.com>
2025-06-14 11:10:54 -0700

    hopefully fixed app loading issue

--- vite-src/vite.config.js
+++ vite-src/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://aidolan.github.io/Nuceria-Project.github.io/app",
 })
